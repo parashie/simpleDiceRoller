fe44c5236e623f213a765478757bdde8ed4f5c6d
Parash <85245101+parashie@users.noreply.github.com>
2022-08-27 09:52:32 -0400

    Update diceRoller.py

--- diceRoller.py
+++ diceRoller.py
@@ -3,12 +3,15 @@ import random
 min = 1
 max = 6
 
-
+#Function dice roller
 def rollDice():
+    #Lets user know code is running
     print("rolling a dice 1-6")
     print("rolling...")
+    #Defined earlier, our minimum and maximum are 1 and 6
     print(random.randint(min, max))
     roll = input("Roll again?")
+    #to be honest I think after you initally say yes itll keep going forever...
     if roll == "yes":
         rollDice()
 
